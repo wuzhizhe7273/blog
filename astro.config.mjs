7982bd2009428a919a8ad194785ab13548d29667
wanderer <1379605319@qq.com>
2023-04-29 16:53:25 +0800

    2023-04-29T16+08:00

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
+import remarkGfm from 'remark-gfm';
+import remarkToc from 'remark-toc';
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
+	markdown:{
+		remarkPlugins:[
+			remarkGfm,
+			remarkToc,
+		],
+	}
 });
---
+++ src/content/blog/2023-04-29-bash和linux命令记录.md
@@ -0,0 +1,13 @@
+---
+title: bash和linux命令记录
+description: ""
+pubDate: 2023-04-29T08:43:00.479Z
+updatedDate: 2023-04-29T08:43:00.481Z
+preview: ""
+draft: true
+tags: []
+categories: []
+---
+# bash和linux命令记录
+
+* `$()` 和 `\`\``都可以将命令结果嵌入另一个命令,我的需要，git提交时用时间作为信息，`git commit -m $(date -I'seconds')`,这个命令精确到秒。
