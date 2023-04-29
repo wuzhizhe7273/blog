4a87ca5e28ca8347b7b0b38eb0fbd85159f40f4a
wanderer <1379605319@qq.com>
2023-04-29 18:05:52 +0800

    设置网站地图

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://lonelyuniverse.netlify.app',
 	integrations: [mdx(), sitemap()],
 	markdown:{
 		remarkPlugins:[
